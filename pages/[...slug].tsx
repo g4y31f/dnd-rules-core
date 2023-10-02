b920618cd779e08bcdc627b221a633245734c4e7
Gradowski <vulo4ka@gmail.com>
2023-10-02 23:53:48 +0300

    Fix formatting

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -1,29 +1,29 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import { getPostBySlug, getAllPosts, getLinksMapping } from '../lib/api'
-import { markdownToHtml } from '../lib/markdownToHtml'
-import type PostType from '../interfaces/post'
-import path from 'path'
-import PostSingle from '../components/blog/post-single'
-import Layout from '../components/misc/layout'
-import { NextSeo } from 'next-seo'
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import { getPostBySlug, getAllPosts, getLinksMapping } from '../lib/api';
+import { markdownToHtml } from '../lib/markdownToHtml';
+import type PostType from '../interfaces/post';
+import path from 'path';
+import PostSingle from '../components/blog/post-single';
+import Layout from '../components/misc/layout';
+import { NextSeo } from 'next-seo';
 
 type Items = {
-  title: string,
-  excerpt: string,
-}
+  title: string;
+  excerpt: string;
+};
 
 type Props = {
-  post: PostType
-  slug: string
-  backlinks: { [k: string]: Items }
-}
+  post: PostType;
+  slug: string;
+  backlinks: { [k: string]: Items };
+};
 
 export default function Post({ post, backlinks }: Props) {
-  const router = useRouter()
-  const description = post.excerpt.slice(0, 155)
+  const router = useRouter();
+  const description = post.excerpt.slice(0, 155);
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
   return (
     <>
@@ -38,12 +38,16 @@ export default function Post({ post, backlinks }: Props) {
               title: post.title,
               description,
               type: 'article',
-              images: [{
-                url: (post.ogImage?.url) ? post.ogImage.url : "https://fleetingnotes.app/favicon/512.png",
-                width: (post.ogImage?.url) ? null: 512,
-                height: (post.ogImage?.url) ? null: 512,
-                type: null
-              }]
+              images: [
+                {
+                  url: post.ogImage?.url
+                    ? post.ogImage.url
+                    : 'https://fleetingnotes.app/favicon/512.png',
+                  width: post.ogImage?.url ? null : 512,
+                  height: post.ogImage?.url ? null : 512,
+                  type: null,
+                },
+              ],
             }}
           />
           <PostSingle
@@ -56,18 +60,18 @@ export default function Post({ post, backlinks }: Props) {
         </Layout>
       )}
     </>
-  )
+  );
 }
 
 type Params = {
   params: {
-    slug: string[]
-    backlinks: string[]
-  }
-}
+    slug: string[];
+    backlinks: string[];
+  };
+};
 
 export async function getStaticProps({ params }: Params) {
-  const slug = path.join(...params.slug)
+  const slug = path.join(...params.slug);
   const post = await getPostBySlug(slug, [
     'title',
     'excerpt',
@@ -76,14 +80,20 @@ export async function getStaticProps({ params }: Params) {
     'author',
     'content',
     'ogImage',
-  ])
-  const content = await markdownToHtml(post.content || '', slug)
-  const linkMapping = await getLinksMapping()
-  const backlinks = Object.keys(linkMapping).filter(k => linkMapping[k].includes(post.slug) && k !== post.slug)
-  const backlinkNodes = Object.fromEntries(await Promise.all(backlinks.map(async (slug) => {
-    const post = await getPostBySlug(slug, ['title', 'excerpt']);
-    return [slug, post]
-  })));
+  ]);
+  const content = await markdownToHtml(post.content || '', slug);
+  const linkMapping = await getLinksMapping();
+  const backlinks = Object.keys(linkMapping).filter(
+    (k) => linkMapping[k].includes(post.slug) && k !== post.slug
+  );
+  const backlinkNodes = Object.fromEntries(
+    await Promise.all(
+      backlinks.map(async (slug) => {
+        const post = await getPostBySlug(slug, ['title', 'excerpt']);
+        return [slug, post];
+      })
+    )
+  );
 
   return {
     props: {
@@ -93,19 +103,19 @@ export async function getStaticProps({ params }: Params) {
       },
       backlinks: backlinkNodes,
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const posts = await getAllPosts(['slug'])
+  const posts = await getAllPosts(['slug']);
   return {
     paths: posts.map((post) => {
       return {
         params: {
           slug: post.slug.split(path.sep),
         },
-      } 
+      };
     }),
     fallback: false,
-  }
+  };
 }
