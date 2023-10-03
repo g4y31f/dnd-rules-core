01d43a217ec5bd15ea73fdad525257cb5a2d3f89
Gradowski <vulo4ka@gmail.com>
2023-10-03 19:59:01 +0300

    Show backlinks text only if there are any

--- components/blog/post-single.tsx
+++ components/blog/post-single.tsx
@@ -5,32 +5,25 @@ import PostBody from './post-body';
 import PostMeta from './post-meta';
 
 type Props = {
-  title: string,
-  content: string,
-  date?: string,
-  author?: Author,
-  backlinks: { [k: string]: {
-      title: string,
-      excerpt: string,
-    }
-  }
-}
+  title: string;
+  content: string;
+  date?: string;
+  author?: Author;
+  backlinks: {
+    [k: string]: {
+      title: string;
+      excerpt: string;
+    };
+  };
+};
 
-function PostSingle({
-  title,
-  date,
-  author,
-  content,
-  backlinks
-}: Props) {
+function PostSingle({ title, date, author, content, backlinks }: Props) {
   return (
     <section>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           <div className="max-w-3xl mx-auto lg:max-w-none">
-
             <article>
-
               {/* Article header */}
               <header className="max-w-3xl mx-auto mb-20">
                 {/* Title */}
@@ -39,46 +32,39 @@ function PostSingle({
 
               {/* Article content */}
               <div className="lg:flex lg:justify-between" data-sticky-container>
-
-
                 {/* Main content */}
                 <div>
-
                   {/* Article meta */}
                   {(author || date) && (
                     <>
-                      <PostMeta author={author} date={date}/>
+                      <PostMeta author={author} date={date} />
                       <hr className="w-16 h-px pt-px bg-gray-200 border-0 my-6" />
                     </>
                   )}
 
                   {/* Article body */}
-                  <PostBody content={content}/>
-
+                  <PostBody content={content} />
                 </div>
 
                 {/* Sidebar */}
-                <hr className="my-10 border border-dashed lg:block"/>
+                <hr className="my-10 border border-dashed lg:block" />
                 <aside className="relative lg:block lg:w-72 lg:ml-20 shrink-0">
-                  <div>
-                    <h4 className="text-lg font-bold leading-snug tracking-tight mb-4">Backlinks</h4>
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4">
-                      {
-                        (Object.keys(backlinks).length > 0) && (
-                            <Backlinks backlinks={backlinks} />
-                        )
-                      }
+                  {Object.keys(backlinks).length > 0 && (
+                    <div>
+                      <h4 className="text-lg font-bold leading-snug tracking-tight mb-4">
+                        Зворотні посилання
+                      </h4>
+                      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4">
+                        <Backlinks backlinks={backlinks} />
+                      </div>
                     </div>
-                  </div>
+                  )}
                 </aside>
-
               </div>
 
               {/* Article footer */}
             </article>
-
           </div>
-
         </div>
       </div>
     </section>
