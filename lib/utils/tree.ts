8bc69bb03c0680c4792f12665bc3323e64eded72
Gradowski <vulo4ka@gmail.com>
2023-11-30 20:55:33 +0200

    Modify tree function

--- lib/utils/tree.ts
+++ lib/utils/tree.ts
@@ -1,16 +1,28 @@
 type TreeNode = { [key: string]: TreeNode | string };
 
-export function buildTree(paths: string[]): TreeNode {
-  const tree: TreeNode = {};
-  paths.forEach((path) => {
-    const parts = path.split('/');
-    let node = tree;
-    parts.forEach((part, i) => {
-      if (!node[part]) {
-        node[part] = i === parts.length - 1 ? path : {};
-      }
-      node = node[part] as TreeNode;
-    });
-  });
-  return tree;
+export function buildTree(
+  paths: string[],
+  node: TreeNode = {},
+  pathIndex: number = 0
+): TreeNode {
+  if (pathIndex >= paths.length) {
+    return node;
+  }
+
+  const path = paths[pathIndex];
+  const parts = path.split('/');
+  let currentNode = node;
+
+  parts.reduce((node, part, i) => {
+    if (typeof node[part] === 'string') {
+      const temp = node[part];
+      node[part] = { [temp]: {} };
+    }
+    if (!node[part]) {
+      node[part] = i === parts.length - 1 ? path : {};
+    }
+    return node[part] as TreeNode;
+  }, currentNode);
+
+  return buildTree(paths, node, pathIndex + 1);
 }
